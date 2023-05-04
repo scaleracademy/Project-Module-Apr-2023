ac556a43b7d1489a79bd9e791de89697ad0123c6
Arnav Gupta <championswimmer@gmail.com>
2023-05-04 08:45:57 +0530

    create empty controllers
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ blogapi/src/main/java/com/scaler/blogapi/users/UsersController.java
@@ -0,0 +1,9 @@
+package com.scaler.blogapi.users;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+}
