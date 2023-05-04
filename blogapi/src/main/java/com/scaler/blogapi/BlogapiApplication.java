fd9d785e1ba021bd46cb61ffd40f0aa9834523af
Arnav Gupta <championswimmer@gmail.com>
2023-05-04 08:43:15 +0530

    blog API basic setup via Spring wizard
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ blogapi/src/main/java/com/scaler/blogapi/BlogapiApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.blogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogapiApplication.class, args);
+    }
+
+}
---
+++ blogapi/src/test/java/com/scaler/blogapi/BlogapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
