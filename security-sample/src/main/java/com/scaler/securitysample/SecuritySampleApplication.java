4d6efb8e45594f704249d21f8e1800b00c8c77d1
Arnav Gupta <championswimmer@gmail.com>
2023-05-18 07:16:51 +0530

    added sample project with spring security enabled
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ security-sample/src/main/java/com/scaler/securitysample/SecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.securitysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySampleApplication.class, args);
+    }
+
+}
---
+++ security-sample/src/test/java/com/scaler/securitysample/SecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.securitysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
